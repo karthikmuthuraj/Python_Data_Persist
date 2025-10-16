8e839db987d206e9c2d0cbe1eca00e709cdadd01
Karthik Muthuraj <53556613+karthikmuthuraj@users.noreply.github.com>
2025-10-16 22:46:41 +0530

    Update Jenkinsfile.py

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -3,7 +3,7 @@ pipeline {
         stages {
             stage('Build') {
                 steps {
-                    bat 'python .\testPy.py'
+                    bat 'python testPy.py'
                 }
             }
         }    
