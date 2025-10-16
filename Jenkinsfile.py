6bf7b541c2e4f125d5571815a70f3bfe394d3a63
Karthik Muthuraj <karthik.muthuraj@gmail.com>
2025-10-16 23:20:32 +0530

    commit-25:

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -2,13 +2,13 @@ pipeline {
     agent any
     environment {
         PATH = "C:\\WINDOWS\\SYSTEM32"
-        python = "C:\\Python312"
+        PYTHONPATH = "C:\\Python312"
     }
 
     stages {
         stage('Build') {
             steps {
-                bat 'pyrun.bat'
+                bat 'python .\testPy.py'
             }
         }
     }    
