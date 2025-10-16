f661957871d34992bdbc04e30fb2936af9b24508
Karthik Muthuraj <karthik.muthuraj@gmail.com>
2025-10-16 22:54:21 +0530

    commit-22:

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -3,7 +3,7 @@ pipeline {
         stages {
             stage('Build') {
                 steps {
-                    powershell 'python testPy.py'
+                    bat  "pyrun.bat"
                 }
             }
         }    
