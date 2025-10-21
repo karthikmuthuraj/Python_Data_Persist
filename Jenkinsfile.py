8d437626097d7207085c7f37215a9ff0854f14d9
Karthik Muthuraj <karthik.muthuraj@gmail.com>
2025-10-22 00:13:32 +0530

    Revert "21Oct2025_04:Chek with CMD for BAT / POWERSHELL"
    This reverts commit 81a4d43d9ded61c5ba63d9be823fb9206c9aa77b.

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Test Python Script') {
             steps {
-                cmd 'python testPy.py'
+                bat 'python testPy.py'
             }
         }
     }    
