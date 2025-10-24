15fede06358aaa0fafbaf5c56d274dd3b3356457
Karthik Muthuraj <karthik.muthuraj@gmail.com>
2025-10-24 11:49:03 +0530

    24Oct2025: Jenkinsfile modification  to trigger powershell script.

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -1,6 +1,16 @@
 pipeline {
     agent any
     stages {
+        '''
+        stage('User Input') {
+            steps {
+                script {
+                    def userInput = input(message: 'Please enter a value:', ok: 'Proceed', submitter: 'admin')
+                    echo "User entered: ${userInput}"
+                }
+            }
+        }
+        '''
         stage('Test Python Script') {
             steps {
                 powershell 'python testPy.py'
