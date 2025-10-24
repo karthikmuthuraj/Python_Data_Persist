d0bd10fc1968f27f51609146343d40703bc0db56
Karthik Muthuraj <karthik.muthuraj@gmail.com>
2025-10-24 12:24:04 +0530

    24Oct025:Jenkins Input option added

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -1,28 +1,24 @@
 pipeline {
     agent any
     stages {
+        stage('Test') {
+        def reg = input(
+            message: 'What is the reg value?', 
+            parameters: [
+                [$class: 'ChoiceParameterDefinition', 
+                    choices: 'Choice 1\nChoice 2\nChoice 3', 
+                    name: 'input', 
+                    description: 'A select box option']
+            ])
+
+        echo "Reg is ${reg}"
+    }
         '''
-        stage('User Input') {
+         stage('Test Python Script') {
             steps {
-                script {
-                    def userInput = input(message: 'Please enter a value:', ok: 'Proceed', submitter: 'admin')
-                    echo "User entered: ${userInput}"
-                }
+                powershell 'python testPy.py'
             }
-        }
+         }
         '''
-        stage('Test Python Script') {
-            steps {
-                script {
-                    def userInput = input(message: 'Please enter a value:', ok: 'Proceed', submitter: 'admin')
-                    echo "User entered: ${userInput}"
-                }
-            }
-        }
-        // stage('Test Python Script') {
-        //    steps {
-        //        powershell 'python testPy.py'
-        //    }
-        // }
     }    
 }
